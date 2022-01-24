89d834949a275b5447153fbccab6e90f9b8cefcb
fcventura02 <filipecvr4@gmail.com>
2022-01-24 02:25:52 -0300

    update favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,12 +2,14 @@ import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../src/css/global";
 import theme from "../src/css/theme";
+import icon from "../src/assets/img/favicon.png"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Head>
+          <link rel="shortcut icon" href={icon.src} type="image/x-icon" />
           <title>Agricultura Digital</title>
           <meta name="description" content="É Natural ser digital." />
         </Head>
