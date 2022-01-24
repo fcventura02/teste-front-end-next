12c75078f8bac609a5ec201511795e94e8ac8a05
fcventura02 <filipecvr4@gmail.com>
2022-01-24 03:11:28 -0300

    implement forms

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import theme from "../src/css/theme";
 import {
   Banner,
@@ -6,15 +7,107 @@ import {
   Container,
   ContainerText,
   Footer,
+  Form,
   Header,
+  Input,
   Paragraph,
   SubTitle,
   Title,
+  Label,
+  Span,
+  Alert,
+  ButtonClose,
 } from "../src/components";
 import imgCabine from "../src/assets/img/cabineVehicle.png";
 import imgGestao from "../src/assets/img/software.png";
+import { getCits, getCitys, getStates } from "../src/service";
 
 const Home = ({ data }) => {
+  const [isOpenAlert, setIsOpenAlert] = useState(false);
+  const [erro, setErro] = useState(["false"]);
+  const [timeAlert, setTimeAlert] = useState();
+  const [name, setName] = useState("");
+  const [tel, setTel] = useState("");
+  const [email, setEmail] = useState("");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [listCity, setListCity] = useState([]);
+  const [product, setProduct] = useState("");
+  const [message, setMessage] = useState("");
+  const [newsLater, setNewsLater] = useState(false);
+  const [terms, setTerms] = useState(false);
+  function handleChangeName(e) {
+    setErro(erro.filter((er) => er !== "name"));
+    setName(e.target.value);
+  }
+  function handleChangeTel(value) {
+    setErro(erro.filter((er) => er !== "tel"));
+    setTel(
+      value
+        .replace(/\D/g, "")
+        .replace(/(\d{2})(\d)/, "($1) $2")
+        .replace(/(\d{1})/, "$1")
+        .replace(/(\d{1})(\d{4})(\d{4})/, "$1 $2-$3")
+    );
+  }
+  function handleChangeEmail(e) {
+    setErro(erro.filter((er) => er !== "email"));
+    setEmail(e.target.value);
+  }
+  async function handleChangeState(e) {
+    setErro(erro.filter((er) => er !== "state"));
+    setState(e.target.value);
+    if (e.target.value.length === 2) {
+      const itemState = data.filter((item) => item.sigla === e.target.value);
+      itemState.length === 1 &&
+        (await getCitys(itemState[0].id).then((res) => {
+          setListCity(res);
+        }));
+    }
+  }
+  function handleChangeCity(e) {
+    setErro(erro.filter((er) => er !== "city"));
+    setCity(e.target.value);
+  }
+  function handleChangeProduct(e) {
+    setErro(erro.filter((er) => er !== "product"));
+    setProduct(e.target.value);
+  }
+  function handleChangeTerms(e) {
+    setErro(erro.filter((er) => er !== "terms"));
+    setTerms(e.target.value);
+  }
+
+  function validadeEmail(_email) {
+    const reg = /\S+@\S+\.\S+/;
+    if (!reg.test(_email)) {
+      return false;
+    }
+    return true;
+  }
+
+  function handleChangeSubmit(e) {
+    e.preventDefault();
+    setErro([
+      name.trim() === "" && "name",
+      tel.trim() === "" && "tel",
+      (email.trim() === "" || !validadeEmail(email)) && "email",
+      state.trim() === "" && "state",
+      city.trim() === "" && "city",
+      product.trim() === "" && "product",
+      !terms && "terms",
+    ]);
+
+    if (erro.length === 0) {
+      setIsOpenAlert(!isOpenAlert);
+      setTimeAlert(
+        window.setTimeout(() => {
+          setIsOpenAlert(false);
+        }, 5000)
+      );
+    }
+  }
+
   return (
     <>
       <Header />
@@ -113,6 +206,207 @@ const Home = ({ data }) => {
               Quer conhecer a solução ideal para o seu campo? Fale agora mesmo
               com o nosso time de especialistas.
             </SubTitle>
+            <Form
+              id="form"
+              onSubmit={handleChangeSubmit}
+              className="container--padding_vertical form"
+            >
+              <Input>
+                <Label htmlFor="name">Nome</Label>
+                <input
+                  value={name}
+                  onChange={handleChangeName}
+                  type="text"
+                  id="name"
+                />
+                <Span
+                  className={`input_error${
+                    erro.toString().includes("name") ? "-open" : ""
+                  }`}
+                  id="erroMessage-name"
+                >
+                  Este campo é obrigatório
+                </Span>
+              </Input>
+              <Input>
+                <Label htmlFor="tel">Telefone</Label>
+                <input
+                  value={tel}
+                  onChange={(e) => handleChangeTel(e.target.value)}
+                  type="tel"
+                  id="tel"
+                  maxLength="16"
+                  minLength="14"
+                />
+                <Span
+                  className={`input_error${
+                    erro.toString().includes("tel") ? "-open" : ""
+                  }`}
+                  id="erroMessage-tel"
+                >
+                  Este campo é obrigatório
+                </Span>
+              </Input>
+              <Input>
+                <Label htmlFor="email">E-mail</Label>
+                <input
+                  value={email}
+                  onChange={handleChangeEmail}
+                  type="email"
+                  id="email"
+                />
+                <Span
+                  className={`input_error${
+                    erro.toString().includes("email") ? "-open" : ""
+                  }`}
+                  id="erroMessage-email"
+                >
+                  Este campo é obrigatório
+                </Span>
+              </Input>
+              <Input className="form_input-sm">
+                <Label htmlFor="state">Estado</Label>
+                <input
+                  value={state}
+                  onChange={handleChangeState}
+                  type="text"
+                  id="state"
+                  maxLength="2"
+                  list="stateList"
+                />
+                <datalist id="stateList">
+                  {data.map((item) => (
+                    <option key={item.id} value={item.sigla}>
+                      {item.nome}
+                    </option>
+                  ))}
+                </datalist>
+                <Span
+                  className={`input_error${
+                    erro.toString().includes("state") ? "-open" : ""
+                  }`}
+                  id="erroMessage-state"
+                >
+                  Este campo é obrigatório
+                </Span>
+              </Input>
+              <Input className="form_input-sm">
+                <Label htmlFor="city">Cidade</Label>
+                <input
+                  value={city}
+                  onChange={handleChangeCity}
+                  type="text"
+                  id="city"
+                  list="cityList"
+                  disabled={listCity.length === 0}
+                />
+                <datalist id="cityList">
+                  {listCity.map((item) => (
+                    <option key={item.id} value={item.nome} />
+                  ))}
+                </datalist>
+                <Span
+                  className={`input_error${
+                    erro.toString().includes("city") ? "-open" : ""
+                  }`}
+                  id="erroMessage-city"
+                >
+                  Este campo é obrigatório
+                </Span>
+              </Input>
+              <Input>
+                <Label htmlFor="products">Selecione um produto:</Label>
+                <input
+                  value={product}
+                  onChange={handleChangeProduct}
+                  type="text"
+                  id="products"
+                  list="productsList"
+                />
+                <datalist id="productsList">
+                  <option value="Monitores"></option>
+                  <option value="Gestão de frota"></option>
+                </datalist>
+                <Span
+                  className={`input_error${
+                    erro.toString().includes("product") ? "-open" : ""
+                  }`}
+                  id="erroMessage-products"
+                >
+                  Este campo é obrigatório
+                </Span>
+              </Input>
+              <Input>
+                <Label htmlFor="message">
+                  Mensagem <Span>(Opcional)</Span>
+                </Label>
+                <textarea
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  id="message"
+                  rows="10"
+                ></textarea>
+              </Input>
+              <Input>
+                <div>
+                  <Label htmlFor="newsLater">
+                    Desejo receber mais informações sobre produtos e serviços da
+                    New Holland
+                  </Label>
+                  <input
+                    checked={newsLater}
+                    onChange={(e) => setNewsLater(e.target.checked)}
+                    id="newsLater"
+                    type="checkbox"
+                  />
+                </div>
+              </Input>
+              <Input>
+                <div>
+                  <Label htmlFor="terms">
+                    Li e concordo com a <b>Política de Privacidade</b> deste
+                    site
+                  </Label>
+                  <input
+                    checked={terms}
+                    onChange={handleChangeTerms}
+                    id="terms"
+                    type="checkbox"
+                  />
+                </div>
+                <Span
+                  className={`input_error${
+                    erro.toString().includes("terms") ? "-open" : ""
+                  }`}
+                  id="erroMessage-terms"
+                >
+                  Este campo é obrigatório
+                </Span>
+              </Input>
+              <Input>
+                <input type="submit" value="Enviar" />
+              </Input>
+            </Form>
+          </Container>
+          <Container>
+            <Alert
+              id="alert"
+              className={isOpenAlert ? "alert-open" : "alert-disabled"}
+            >
+              <ButtonClose
+                onClick={() => {
+                  window.clearInterval(timeAlert);
+                  setIsOpenAlert(!isOpenAlert);
+                }}
+              />
+              <SubTitle theme={{ textColor: theme.colors.yellow }}>
+                Obrigado pelo seu contato!
+              </SubTitle>
+              <Paragraph>
+                Assim que um de nossos especialistas vizualizar sua mensagem,
+                entraremos em contato.
+              </Paragraph>
+            </Alert>
           </Container>
         </section>
       </main>
@@ -120,5 +414,14 @@ const Home = ({ data }) => {
     </>
   );
 };
+
+export async function getStaticProps() {
+  const { data } = await getStates();
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
 
 export default Home;
